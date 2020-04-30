18989853aa536b7e7053a01acd2819ac0a045174
kadepeay <kadepeay@gmail.com>
2020-04-30 15:58:30 -0600

    Menu implemented

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ the program then finds the address of the person named"""
 def All():
     f = open('doc/AddressBook.txt', "r")
     for line in f:
-        print(line, end='')
+        print(line)
     f.close
 
 # Adds name and address to file
@@ -21,45 +21,64 @@ def Add(N, A):
     f.close
     print("Wrote to 'AddressBook.txt'")
 
-# Returns the address of one person
-def OnePerson():
-    Dict = {}
-    f = open('doc/AddressBook.txt', 'r')
-    for line in f:
-        re.split(r'[,:]', line)
-        if line not in Dict:
-            dict[line] = name
-    f.close
+# Returns the name provided the address
+def OnePerson(A):
+    Dict = CreateDict()
+    for key, value in Dict:
+        if A == value:
+            return key
 
+
+# Returns the address provided the name
+def OneAddress(N):
+    pass
+
+# First create the dictionary and then use the other functions to search
 def CreateDict():
     Dict = {}
     f = open('doc/AddressBook.txt', 'r')
     for line in f:
-        re.split(r'[,:]', line)
+        # line.strip('\n')
         if line not in Dict:
-            dict[line] = line
+            strip = line.split(':', 1)
+            Dict[strip[0]] = strip[1]
     f.close
-
-# Returns the name of one address
-def OneAddress():
-    pass
+    return Dict
 
 # Deletes one person and their address
 def Delete():
     pass
 
+
+def Menu():
+    print()
+    print("Choose one: All, Add, OnePerson, OneAddress")
+    print("To exit enter X")
+    print()
+    option = input(">? ")
+    print()
+    if option.lower() == 'x':
+        quit()
+    elif option.lower() == 'all':
+        print()
+        All()
+    elif option.lower() == 'add':
+        name = input("Enter name: ") 
+        address = input("Enter address: ")
+        print()
+        Add(name, address)
+    elif option.lower() == 'oneperson':
+        address = input("Enter address: ")
+        print()
+        OnePerson(address)
+    else:
+        print("Not valid command, try again")
+        print()
+
+        
+    Menu()
+    
+
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-        print("Choose one: All, Add, OnePerson, OneAddress, Delete")
-    else:
-        if sys.argv[1].lower() == 'all':
-            print()
-            All()
-        elif sys.argv[1].lower() == 'add':
-            name = input("Enter name: ") 
-            address = input("Enter address: ")
-            print()
-            Add(name, address)
-        elif sys.argv[1].lower() == 'dict':
-            CreateDict()
+    Menu()
